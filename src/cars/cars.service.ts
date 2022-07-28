ad4e9a2db52946b5df840c1922a814e1d19d9404
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-07-27 20:41:31 -0500

    section-3-primeros pasos

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+
+  private cars = [
+    { id: 1, name: 'Toyota', model: 'Corolla' },
+    { id: 2, name: 'Honda', model: 'Civic' },
+    { id: 3, name: 'Ford', model: 'Focus' },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find(car => car.id === id);
+
+    if (!car) {
+      throw new NotFoundException(`Car with id '${id}' not found`);
+    }
+
+    return car;
+  }
+}
